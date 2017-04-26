d84d8b663bd87d6b9778425ce2ffa33b8dcf0852
chesteraustin <chesteraustin@gmail.com>
2017-04-25 23:22:12 -0700

    put letters into cards

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,39 @@
-// A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( "ready!" );
+	//Put letters into cards
+	for(var i = 65; i < 91; i++){
+		var myLetter = String.fromCharCode(i)
+		$("#letterCards").append(''
+			+'<div id="letter_'+ myLetter +'" class="card card-outline-primary mb-3 text-center">'
+			+	'<div class="card-block selectedLetter">'
+			+		'<blockquote class="card-blockquote">'
+			+			'<h1>'+ myLetter + '</h1>'
+			+		'</blockquote>'
+			+	'</div>'
+			+'</div>'
+			)
+	}
+
+	var myWord = pickWord();
+	displayWord(myWord);
+
+
+	function pickWord(){
+		var currentWord = "hello world";
+		return currentWord;
+	}
+
+	function displayWord(selectedWord){
+		for(var i = 0; i < selectedWord.length; i++){
+			this.letter = selectedWord.charAt(i);
+			console.log(this.letter);
+			if (this.letter != ' ') {
+				$("#hiddenWord").append(" _ ");
+			}
+			else {
+				$("#hiddenWord").append(" - ");
+			}
+
+		}
+	}
+
 });
